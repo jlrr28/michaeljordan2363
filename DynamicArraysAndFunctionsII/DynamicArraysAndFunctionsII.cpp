ff619eb3910ae5dc43f34d8ca14a718f6f69f2be
J R <jlrr28email@gmail.com>
2026-09-12 22:13:23 -0700

    intresting

--- DynamicArraysAndFunctionsII/DynamicArraysAndFunctionsII.cpp
+++ DynamicArraysAndFunctionsII/DynamicArraysAndFunctionsII.cpp
@@ -17,13 +17,18 @@ int main()
 	const int SIZE = 5;
 	string* dStr = nullptr;
 	dStr = new string[SIZE];
-	*dStr = ("what", "when", "where", "how", "why");
-	dStr[1] = "what";
-
+	*dStr = "what", "when", "where", "how", "why";
 
 	for (int i = 0; i < SIZE; i++)
 		cout << i << " " << dStr[i] << "  " << &dStr[i] << endl;
+	cout << endl;
+	dStr[1] = "when";
+	dStr[2] = "when";
+	dStr[3] = "when";
+	dStr[4] = "when";
 
+	for (int i = 0; i < SIZE; i++)
+		cout << i << " " << dStr[i] << "  " << &dStr[i] << endl;
 
 	delete[] dStr;
 
