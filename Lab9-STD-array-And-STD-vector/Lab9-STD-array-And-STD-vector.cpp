88ebcf66964be71bcf03ea5d2a62a616da7a2d9b
J R <jlrr28email@gmail.com>
2026-09-18 14:45:13 -0700

    first

---
+++ Lab9-STD-array-And-STD-vector/Lab9-STD-array-And-STD-vector.cpp
@@ -0,0 +1,18 @@
+// COMSC 210 || LAB 9 || Jose Luis Ramos
+
+#include <iostream>
+#include <algorithm>  
+#include <numeric>
+#include <array>
+using namespace std;
+
+int main() {
+
+	const int SIZE = 30;
+
+	array<int, 30>scoreArray;
+
+
+
+
+}
---
+++ pointerPlay/pointerPlay.cpp
@@ -0,0 +1,83 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+string* reverseArray(int s, string* dS);
+
+void displayArray(int s, string* dS);
+
+int main()
+{
+	const int SIZE = 5;
+	string* dStr = nullptr;
+	dStr = new string[SIZE];
+	
+	
+	cout << endl << "memory address check" << endl;
+	for (int i = 0; i < SIZE; i++)
+		cout << i << " " << dStr[i] << "  " << &dStr[i] << endl;
+	cout << "------------------" << endl << endl;
+
+	
+	dStr[0] = "what";
+	dStr[1] = "when";
+	dStr[2] = "where";
+	dStr[3] = "how";
+	dStr[4] = "why";
+
+	cout << "initial" << endl;
+	displayArray(SIZE, dStr);
+	
+
+
+	dStr = reverseArray(SIZE, dStr);
+	
+
+
+	cout << "first reverse" << endl;
+	displayArray(SIZE, dStr);
+	/*
+	displayArray(SIZE, dStr);
+	dStr = reverseArray(SIZE, dStr);
+	cout << "second reverse" << endl;
+	displayArray(SIZE, dStr);
+	dStr = reverseArray(SIZE, dStr);
+	cout << "third reverse" << endl;
+	displayArray(SIZE, dStr);*/
+
+	delete[] dStr;
+
+}
+
+
+string* reverseArray(int s, string* dS) {
+	string* reverse = nullptr;
+	reverse = new string[s];
+	for (int i = 0, x = s - 1; i < s; i++, x--)
+		dS[x] = reverse[i];
+
+	// Leaving this in for future reference for myself
+	cout << "dS[] inside reverseArray function" << endl;
+	for (int i = 0; i < s; i++)
+		cout << i << " " << dS[i] << "  " << &dS[i] << endl;
+
+	cout << "reverse[] inside reverseArray function" << endl;
+	for (int i = 0; i < s; i++)
+		cout << i << " " << reverse[i] << "  " << &dS[i] << endl;
+	
+
+	return dS;
+
+	delete[]reverse;
+
+	cout << "reverse[] inside reverseArray function" << endl;
+	for (int i = 0; i < s; i++)
+		cout << i << " " << reverse[i] << "  " << &dS[i] << endl;
+
+	delete[]dS;// not sure if neccsary, leaving this to be safe, tried other configurations but they weren't quite right, will ask about it on discussion board or zoom
+}
+
+void displayArray(int s, string* dS) {
+	for (int i = 0; i < s; i++)
+		cout << i << " " << dS[i] << "  " << &dS[i] << endl;
+}
