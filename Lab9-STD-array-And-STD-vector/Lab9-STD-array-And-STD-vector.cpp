b3b030dda4381c83eab6bd46fb6b3f42dae9c4d7
J R <jlrr28email@gmail.com>
2026-09-18 20:05:32 -0700

    oops

--- Lab9-STD-array-And-STD-vector/Lab9-STD-array-And-STD-vector.cpp
+++ Lab9-STD-array-And-STD-vector/Lab9-STD-array-And-STD-vector.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>  
 #include <numeric>
 #include <array>
+#include <vector>
 #include <fstream>
 #include <iomanip>
 using namespace std;
@@ -18,7 +19,6 @@ int main() {
     ifstream fin;
     fin.open("scores.txt");
     if (fin.good()) {
-        cout << "good to go" << endl;
         int i = 0;
         while (fin >> temp){
             scoreArray[i] = temp;
@@ -29,13 +29,6 @@ int main() {
     }
     else
         cout << "File not found.\n";
-
-    //cout << scoreArray[0] << endl;
-    //cout << scoreArray[20] << endl;
-    //cout << scoreArray[29] << endl;
-    //cout << scoreArray.data() << endl;
-
-
 
     //point summary
     cout << "The point total is " << accumulate(scoreArray.begin(), scoreArray.end(), 0)
@@ -58,26 +51,77 @@ int main() {
     << " with " << target << " points being scored. " << endl << endl;
     
     // Display sorted score list with game played
-    cout << "Score list top to bottom" << setfill('#') << setw(30); cout << endl << "\n";
+    cout << "Score list top to bottom" << setfill('#') << setw(25); cout << endl << "\n";
     array<int, SIZE> sortedArray = scoreArray;
     sort(sortedArray.begin(), sortedArray.end());
     
     for (int i = scoreArray.size(); i >= 1; i--) {
-        target = sortedArray.at(i-1);
         
+        target = sortedArray.at(i-1);
         it = find(scoreArray.begin(), scoreArray.end(), target);
-        //if ( == target)
-            //it = find(scoreArray.end(), scoreArray.begin(), target);
+        
         cout << "Game:" << (it - scoreArray.begin()) + 1;
-        cout << ((((it - scoreArray.begin()) + 1) < 10) ? "  ||| " : " ||| ");
-        cout << target << " points" << endl;
+        cout << ((((it - scoreArray.begin()) + 1) < 10) ? "   |||  " : "  |||  ");
+        cout << target;
+        cout << ((target <= 100) ? "  points" : " points") << endl;
+       // " points" << endl;
 
     }
     
     cout << " ------------- same code but with vector ------------- " << endl;
 
+    vector<int> scoreVector;
     
-    //for (int val : sortedArray) cout << val << " " << endl;
+    fin.open("scores.txt");
+    if (fin.good()) {
+        int i = 0;
+        while (fin >> temp) {
+            scoreVector.push_back(temp);
+            //temp = scoreVector.at(i);
+            i++;
+        }
 
+        fin.close();
+    }
+    else
+        cout << "File not found.\n";
+
+    // point summary
+        cout << "The point total is " << accumulate(scoreVector.begin(), scoreVector.end(), 0)
+        << " points over " << scoreVector.size() << " games played." << endl;;
+    cout << "The average score of the " << scoreVector.size() << " games played is "
+        << static_cast<double>(accumulate(scoreVector.begin(), scoreVector.end(), 0)) / scoreVector.size()
+        << " points" << endl;
+
+    //Find lowest score game
+    target = *min_element(scoreVector.begin(), scoreVector.end());
+    vector<int>::iterator itV;
+    itV = find(scoreVector.begin(), scoreVector.end(), target);
+    cout << "The lowest score was on game " << (it - scoreArray.begin()) + 1 <<
+        " with " << target << " points being scored. " << endl;
+
+    //Find highest score game
+    target = *max_element(scoreVector.begin(), scoreVector.end());
+    itV = find(scoreVector.begin(), scoreVector.end(), target);
+    cout << "The highest score was on game " << (itV - scoreVector.begin()) + 1
+        << " with " << target << " points being scored. " << endl << endl;
+
+    // Display sorted score list with game played
+    cout << "Score list top to bottom" << setfill('#') << setw(25); cout << endl << "\n";
+    array<int, SIZE> sortedArray = scoreArray;
+    sort(sortedArray.begin(), sortedArray.end());
+
+    for (int i = scoreArray.size(); i >= 1; i--) {
+
+        target = sortedArray.at(i - 1);
+        it = find(scoreArray.begin(), scoreArray.end(), target);
+
+        cout << "Game:" << (it - scoreArray.begin()) + 1;
+        cout << ((((it - scoreArray.begin()) + 1) < 10) ? "   |||  " : "  |||  ");
+        cout << target;
+        cout << ((target <= 100) ? "  points" : " points") << endl;
+        // " points" << endl;
+
+    }
 
 }
