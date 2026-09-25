fa710a2efa1205f979b30bd057f2ed8385f47211
J R <jlrr28email@gmail.com>
2026-09-24 22:22:44 -0700

    setup

---
+++ Lab13-Student_Grade_Sorter/Lab13-Student_Grade_Sorter.cpp
@@ -0,0 +1,13 @@
+// COMSC - 210 || Lab 11 || Jose Luis Ramos
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+
+struct stuData {
+	int ID;
+	double Grade;
+};
