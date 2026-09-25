68a5e83859a657b0cbbc97d45828006c65798ce5
J R <jlrr28email@gmail.com>
2026-09-24 22:29:35 -0700

    ok

--- Lab13-Student_Grade_Sorter/Lab13-Student_Grade_Sorter.cpp
+++ Lab13-Student_Grade_Sorter/Lab13-Student_Grade_Sorter.cpp
@@ -4,10 +4,39 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <array>
 using namespace std;
 
 
-struct stuData {
+struct sData {
 	int ID;
 	double Grade;
 };
+
+int getFileLines(string f) {
+
+
+    ifstream fin;
+    fin.open(f);
+    if (fin.good()) {
+
+
+
+
+
+        fin.close();
+    }
+    else
+        cout << "File not found.\n";
+
+}
+
+
+int main() {
+
+    string file = "shortgrades.txt";
+	int numberOfStudents = getFileLines();
+	array<sData, 2>studentData;
+
+
+}
